d170a646064892c0d8b3f8262776c9f10c56dfdd
AlexVko <ale@alexvko.com>
2019-05-18 16:32:23 -0300

    Implement functionality

--- lib/hash_to_model_mapper.rb
+++ lib/hash_to_model_mapper.rb
@@ -1,6 +1,59 @@
-require "hash_to_model_mapper/version"
+# frozen_string_literal: true
+
+require 'hash_to_model_mapper/version'
+
+require 'active_support/core_ext/hash'
+require 'active_support/core_ext/string'
 
 module HashToModelMapper
-  class Error < StandardError; end
-  # Your code goes here...
+  @registry = {}
+
+  def self.register(model_name, type, mapper)
+    @registry[model_name] ||= {}
+    @registry[model_name][type] = mapper
+  end
+
+  def self.registry
+    @registry
+  end
+
+  def self.define(&block)
+    definition_proxy = DefinitionProxy.new
+    definition_proxy.instance_eval(&block)
+  end
+
+  def self.call(model_name, type = nil, hash)
+    instance = model_name.to_s.classify.constantize.new
+    instance.readonly!
+    mapper = registry[model_name][type]
+    attributes = mapper.attributes
+    hash = hash.with_indifferent_access
+
+    attributes.each do |attribute_name, path|
+      value = hash.dig(*path)
+      instance.__send__("#{attribute_name}=", value)
+    end
+
+    instance
+  end
+end
+
+class DefinitionProxy
+  def mapper(model_name, type: :none, &block)
+    mapper = Mapper.new
+    mapper.instance_eval(&block)
+    HashToModelMapper.register(model_name, type, mapper)
+  end
+end
+
+class Mapper
+  def initialize
+    @attributes = {}
+  end
+
+  attr_reader :attributes
+
+  def method_missing(name, *path)
+    @attributes[name] = path
+  end
 end
--- spec/hash_to_model_mapper_spec.rb
+++ spec/hash_to_model_mapper_spec.rb
@@ -1,9 +1,44 @@
+# frozen_string_literal: true
+
+class Ad
+  attr_accessor :title, :description, :cover_url
+
+  # Simulating ActiveRecord #readonly!
+  def readonly!
+    true
+  end
+end
+
 RSpec.describe HashToModelMapper do
-  it "has a version number" do
-    expect(HashToModelMapper::VERSION).not_to be nil
+  before do
+    described_class.define do
+      mapper :ad, type: :a do
+        title 'Title'
+        description 'Details', 'Desc'
+        cover_url 'Photos', 0
+      end
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:hash) do
+    {
+      'Title': 'the title',
+      'Details': { 'Desc': 'the description' },
+      'Photos': %w[photo_1 photo_2]
+    }
+  end
+
+  subject { described_class.call(:ad, :a, hash) }
+
+  it 'maps one level attributes' do
+    expect(subject.title).to eq('the title')
+  end
+
+  it 'maps nested attributes' do
+    expect(subject.description).to eq('the description')
+  end
+
+  it 'maps array elements' do
+    expect(subject.cover_url).to eq('photo_1')
   end
 end
