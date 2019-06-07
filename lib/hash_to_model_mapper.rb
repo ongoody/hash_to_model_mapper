9bef454d8cd2ff37a71baf6691de4db398d69930
AlexVKO <ale@alexvko.com>
2019-06-07 18:25:07 -0300

    Add support to receive a model as data source

--- lib/hash_to_model_mapper.rb
+++ lib/hash_to_model_mapper.rb
@@ -34,26 +34,45 @@ module HashToModelMapper
     definition_proxy.instance_eval(&block)
   end
 
-  def self.call(model_name, type = nil, hash)
-    fail("hash needs to be present") unless hash.present?
+  def self.call(model_name, type = nil, source)
+    fail("source needs to be present") unless source.present?
 
     instance = model_name.to_s.classify.constantize.new
     instance.readonly!
     mapper = registry[model_name][type] || fail("Mapper not defined for #{model_name} -> #{type}")
     attributes = mapper.attributes
-    hash = hash.with_indifferent_access
+
+    case source
+    when Hash
+      source = source.with_indifferent_access
+      get_value = ->(path) { source.dig(*path) }
+    when ApplicationRecord 
+      get_value = ->(path) { path.reduce(source) { |source, method| source.__send__(method) } }
+    else
+      fail("Type not supported Hash/ApplicationRecord, however it was #{source.class}: #{source}")
+    end
 
     attributes.each do |attribute_name, path|
-      value = hash.dig(*path)
+      value = get_value.(path)
 
       if (transformer = mapper.transformers[attribute_name])
-        value = transformer.call(value)
+        if transformer.is_a? Hash
+          value = transformer.with_indifferent_access[value] || fail("Key not present in tansformer: #{value}")
+        elsif transformer.respond_to? :call
+          value = transformer.call(value)
+        else
+          fail('transformer is neither a hash or a callable object')
+        end
       end
       instance.__send__("#{attribute_name}=", value)
     end
 
+    instance.id = nil if instance.respond_to? :id
+
     instance
   end
+
+  private
 end
 
 class DefinitionProxy
